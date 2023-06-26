486598506e014bf3c2110c49dcdf69b781bb6b69
logicallaw <logicallawbio@gmail.com>
2023-06-26 10:07:59 +0900

    jwt 토큰 구현

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const authRouter = require('./routes/auth');
+const v1Router = require('./routes/v1')
 const indexRouter = require('./routes');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
@@ -48,6 +49,7 @@ app.use(passport.session());
 
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
+app.use('/v1',v1Router)
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
